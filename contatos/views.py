87a3873510eb9bbc1682f904048da6c12216bd9d
Caio Sella <sellacaio4@gmail.com>
2023-02-19 00:01:01 -0300

    Campo de busca adicionado

--- contatos/views.py
+++ contatos/views.py
@@ -1,4 +1,6 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
 
@@ -6,7 +8,9 @@ from .models import Contato
 
 
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True
+    )
     paginator = Paginator(contatos, 6)
 
     page = request.GET.get('p')
@@ -20,6 +24,33 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+
+    if termo is None or not termo:
+        raise Http404()
+
+    campos = Concat('nome', Value(' '), 'sobrenome')
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos
+    ).filter(
+        Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )
+    paginator = Paginator(contatos, 6)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
