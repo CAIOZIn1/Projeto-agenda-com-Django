5223405c35bfcb3001dcc2728e8393b4f2876403
Caio Sella <sellacaio4@gmail.com>
2023-02-20 00:26:38 -0300

    paginas para login adicionadas

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.contrib import messages
+from django.shortcuts import render
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def cadastro(request):
+    messages.success(request, 'Formulário enviado!')
+    return render(request, 'accounts/cadastro.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
